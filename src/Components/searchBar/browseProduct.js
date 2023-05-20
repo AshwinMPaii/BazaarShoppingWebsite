a7ec96e3c088b5e62ca8c56af74a3453bc93c762
Ismail <ismailafrid313@gmail.com>
2023-05-20 22:54:19 +0530

    modal7

--- src/Components/searchBar/browseProduct.js
+++ src/Components/searchBar/browseProduct.js
@@ -1,57 +1,125 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './browseProduct.css';
+import React, { useState } from "react";
+import "./browseProduct.css";
+import shoeImg1 from "../../Assets/Images/shoepic.PNG";
+import shoeImg2 from "../../Assets/Images/shoe2.png";
 
 const BrowseProduct = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-    const handleChange = async (event) => {
-        const { value } = event.target;
-        setSearchTerm(value);
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 9.99,
+      imageSrc: shoeImg1,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 14.99,
+      imageSrc: shoeImg2,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 9.99,
+      imageSrc: "path/to/image1.jpg",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 14.99,
+      imageSrc: "path/to/image2.jpg",
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: 9.99,
+      imageSrc: "path/to/image1.jpg",
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      price: 14.99,
+      imageSrc: "path/to/image2.jpg",
+    },
+    {
+      id: 7,
+      name: "Product 7",
+      price: 9.99,
+      imageSrc: "path/to/image1.jpg",
+    },
+    {
+      id: 8,
+      name: "Product 8",
+      price: 14.99,
+      imageSrc: "path/to/image2.jpg",
+    },
+    {
+      id: 9,
+      name: "Product 9",
+      price: 9.99,
+      imageSrc: "path/to/image1.jpg",
+    },
+    {
+      id: 10,
+      name: "Product 10",
+      price: 14.99,
+      imageSrc: "path/to/image2.jpg",
+    },
+    // Add more products as needed
+  ];
 
-        try {
-            const response = await axios.get(`/api/suggestions?searchTerm=${value}`);
-            const fetchedSuggestions = response.data;
-            setSuggestions(fetchedSuggestions);
-        } catch (error) {
-            console.error('Error fetching suggestions:', error);
-        }
-    };
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
 
-    const handleSelect = (selectedProduct) => {
-        // Implement your logic to handle the selection of a product
-        // You can perform any necessary actions here, such as redirecting to the product page
-        console.log('Selected Product:', selectedProduct);
-    };
-
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            // Implement your logic to handle the Enter key press
-            // You can perform any necessary actions here, such as redirecting to the selected product page
-            console.log('Search Term:', searchTerm);
-        }
-    };
-
-    return (
-        <div className="search-bar-container">
-            <input
-                className='search-bar'
-                type="text"
-                value={searchTerm}
-                onChange={handleChange}
-                onKeyPress={handleKeyPress}
-                placeholder="Search..."
-            />
-            <ul>
-                {suggestions.map((product) => (
-                    <li key={product.id} onClick={() => handleSelect(product)}>
-                        {product.name}
-                    </li>
-                ))}
-            </ul>
-        </div>
+    const filteredSuggestions = products.filter((product) =>
+      product.name.toLowerCase().includes(value.toLowerCase())
     );
+    setSuggestions(filteredSuggestions);
+  };
+
+  const handleSelect = (selectedProduct) => {
+    console.log("Selected Product:", selectedProduct);
+    // Implement your logic to handle the selection of a product
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      console.log("Search Term:", searchTerm);
+      // Implement your logic to handle the Enter key press
+    }
+  };
+
+  return (
+    <div className="search-bar-container">
+      <input
+        className="search-bar"
+        type="text"
+        value={searchTerm}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+        placeholder="Search..."
+      />
+      {suggestions.length > 0 && (
+        <div className="search-suggestions">
+          {suggestions.map((product) => (
+            <div
+              key={product.id}
+              className="suggestion-item"
+              onClick={() => handleSelect(product)}
+            >
+              <img src={product.imageSrc} alt={product.name} />
+              <span>{product.name}</span>
+              <span>${product.price}</span>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default BrowseProduct;
--- src/Components/searchBar/signUp.js
+++ src/Components/searchBar/signUp.js
@@ -4,249 +4,188 @@ import logo from "../../Assets/Icons/logo.png";
 import React, { useState } from "react";
 import "./signUp.css";
 
-
 const SignUp = (props) => {
-    // const onClose = () => {
-    //     props.setIsModalOpen(false);
-    // };
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
 
-    const [isLoginModalOpen, setIsLoginModalOpen] = useState(true);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isEmailValid, setIsEmailValid] = useState(true);
-    const [isPasswordValid, setIsPasswordValid] = useState(true);
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [isSignUp, setIsSignUp] = useState(false);
-    // const [errorMessage, setErrorMessage] = useState('');
+  const handleCloseModal = () => {
+    props.onClose();
+    setIsLoginModalOpen(true);
+  };
 
+  const handleSignUpClick = () => {
+    setIsSignUp(true);
+    setIsLoginModalOpen(false);
+  };
 
-    const handleCloseModal = () => {
-        props.onClose();
-        setIsLoginModalOpen(true);
-        // setEmail('');
-        // setPassword('');
-        // setConfirmPassword('');
-    };
+  const handleLogInClick = () => {
+    setIsSignUp(false);
+    setIsLoginModalOpen(true);
+  };
 
-    const handleSignUpClick = () => {
-        setIsSignUp(true);
-        setIsLoginModalOpen(false);
-    };
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    // Existing sign up logic
+  };
 
-    const handleLogInClick = () => {
-        setIsSignUp(false);
-        setIsLoginModalOpen(true);
-    };
+  const handleLogIn = (event) => {
+    event.preventDefault();
+    // Existing log in logic
+  };
 
-    const handleSignUp = (event) => {
-        event.preventDefault();
-        console.log("Sign up clicked!");
-
-        const isValidEmail = /\S+@gmail\.com/.test(email);//returns true or false
-        setIsEmailValid(isValidEmail);
-        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        const isValidPassword = regex.test(password);
-        setIsPasswordValid(isValidPassword);
-        if (isValidEmail && isValidPassword) {
-            if (password === confirmPassword) {
-
-                const signUpData = {
-                    email,
-                    password
-                };
-
-                fetch("http://localhost:8080/users/add", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(signUpData)
-                })
-                    .then(response => {
-                        // Handle the response from the backend
-                        if (response.ok) {
-                            // Login successful, handle accordingly
-                            response.text().then(data => {
-                                console.log("SignUp successful. Response:", data);
-
-                            });
-
-                        } else {
-                            // Login failed, handle accordingly
-                            alert("Sign Up failed");
-                        }
-                    }).catch(error => {
-                        alert("connection error!!!")
-                    });
-
-
-            }
-            else {
-                alert("password doesn't match");
-            }
-            setEmail('');
-            setPassword('');
-            setConfirmPassword('');
-        } else {
-            alert("Invalid email or password")
-        }
-
-    };
-
-    const handleLogIn = (event) => {
-        event.preventDefault();
-        const loginData = {
-            firstname: email,
-            password
-        };
-
-        // Send the login data to the backend
-        fetch("http://localhost:8080/users/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(loginData)
+  const forgotPassword = () => {
+    const email = prompt("Enter your email address");
+    if (email) {
+      // Send an email to the user with a link to reset their password
+      fetch(`http://localhost:8080/users/forgot-password?email=${email}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          // Handle the response from the backend
+          if (response.ok) {
+            // Email sent successfully, handle accordingly
+            alert(
+              "An email has been sent to you with a link to reset your password"
+            );
+          } else {
+            // Email failed to send, handle accordingly
+            alert("An error occurred while sending the email");
+          }
         })
-            .then(response => {
-                // Handle the response from the backend
-                if (response.ok) {
-                    // Login successful, handle accordingly
-                    response.json().then(data => {
-                        const token = data.token;
-                        console.log("Login successful. Token:", token);
-                    })
-                        .catch(error => {
-                            alert("connection error!!!")
-                        });
+        .catch((error) => {
+          alert("connection error!!!");
+        });
+    }
+  };
 
-
-                } else {
-                    // Login failed, handle accordingly
-                    console.log("Login failed");
-                }
-            });
-    };
-
-
-    return (
-        <>
-
-            <div className="modal">
-                <div className="modal-content">
-                    <button onClick={handleCloseModal} className="close-button">
-                        X
-                    </button>
-                    {isLoginModalOpen ? (
-                        <>
-                            <div
-                                className="login-form"
-                                style={{
-                                    backgroundImage: `url(${backgroundImageLogin})`,
-                                    backgroundSize: "cover",
-                                    backgroundPosition: "center",
-                                }}
-                            >
-                                <div className="header2">
-                                    <img src={logo} alt="Bazzar Logo" className="modallogo" />
-                                    <br></br>
-                                    <p>Welcome to Bazzar</p>
-                                </div>
-
-                                <form>
-                                    <h3>Log In</h3>
-                                    <label htmlFor="email">Email or phone:</label>
-                                    <input
-                                        type="text"
-                                        id="email"
-                                        name="email"
-                                        required
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        className="modalinput"
-                                    />
-                                    <label htmlFor="password">Password:</label>
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        required
-                                        value={password}
-                                        className="modalinput"
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                    <div className="button-container">
-                                        <button onClick={handleLogIn} className="button-login">
-                                            Log In
-                                        </button>
-                                    </div>
-                                    <p>
-                                        Don't have an account?{" "}
-                                        <span onClick={handleSignUpClick}>Sign up</span>
-                                    </p>
-                                </form>
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <div className="signup-form">
-                                <div className="image-container">
-                                    <img src={signUp} alt="imagehere" />
-                                </div>
-                                <form onSubmit={handleSignUp}>
-                                    <h3>Sign Up</h3>
-                                    {/* {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>} */}
-                                    <label htmlFor="email">Email or phone:</label>
-                                    <input
-                                        type="text"
-                                        id="email"
-                                        name="email"
-                                        required
-                                        value={email}
-                                        className="modalinput"
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                    <label htmlFor="password">Password:</label>
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        required
-                                        value={password}
-                                        className="modalinput"
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                    {/* {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-                                    {!isPasswordValid && (
-                                        <p style={{ color: 'red' }}>Invalid Password</p>
-                                    )} */}
-                                    <label htmlFor="confirm-password">Confirm Password:</label>
-                                    <input
-                                        type="password"
-                                        id="confirm-password"
-                                        name="confirm-password"
-                                        required
-                                        value={confirmPassword}
-                                        className="modalinput"
-                                        onChange={(e) => setConfirmPassword(e.target.value)}
-                                    />
-                                    <div className="button-container">
-                                        <button onClick={handleSignUp} className="button-login">
-                                            Sign Up
-                                        </button>
-                                    </div>
-                                    <p>
-                                        Already have an account?{" "}
-                                        <span onClick={handleLogInClick}>Log in</span>
-                                    </p>
-                                </form>
-                            </div>
-                        </>
-                    )}
+  return (
+    <>
+      <div className="modal">
+        <div className="modal-content">
+          <button onClick={handleCloseModal} className="close-button">
+            X
+          </button>
+          {isLoginModalOpen ? (
+            <>
+              <div
+                className="login-form"
+                style={{
+                  backgroundImage: `url(${backgroundImageLogin})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <div className="header2">
+                  <img src={logo} alt="Bazzar Logo" className="modallogo" />
+                  <br></br>
+                  <p>Welcome to Bazzar</p>
                 </div>
-            </div>
-        </>
-    )
+
+                <form>
+                  <h3>Log In</h3>
+                  <label htmlFor="email">Email or phone:</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="modalinput"
+                  />
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="modalinput"
+                  />
+                  <div className="button-container">
+                    <button onClick={handleLogIn} className="button-login">
+                      Log In
+                    </button>
+                  </div>
+                  <p>
+                    Don't have an account?{" "}
+                    <span onClick={handleSignUpClick}>Sign up</span>
+                  </p>
+                  <p>
+                    Forgot password?{" "}
+                    <span onClick={forgotPassword}>Click here</span>
+                  </p>
+                </form>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="signup-form">
+                <div className="image-container">
+                  <img src={signUp} alt="imagehere" />
+                </div>
+                <form onSubmit={handleSignUp}>
+                  <h3>Sign Up</h3>
+                  <label htmlFor="email">Email or phone:</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="modalinput"
+                  />
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="modalinput"
+                  />
+                  <label htmlFor="confirm-password">Confirm Password:</label>
+                  <input
+                    type="password"
+                    id="confirm-password"
+                    name="confirm-password"
+                    required
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    className="modalinput"
+                  />
+                  <div className="button-container">
+                    <button onClick={handleSignUp} className="button-login">
+                      Sign Up
+                    </button>
+                  </div>
+                  <p>
+                    Already have an account?{" "}
+                    <span onClick={handleLogInClick}>Log in</span>
+                  </p>
+                  <p>
+                    Forgot password?{" "}
+                    <span onClick={forgotPassword}>Click here</span>
+                  </p>
+                </form>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
